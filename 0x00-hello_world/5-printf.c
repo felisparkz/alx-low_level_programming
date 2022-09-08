77c952d937ed696462446cde2b01653d90208f27
Felix <felisparkz@gmail.com>
2022-09-08 16:39:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the function
+ *
+ * Description: using the main function 
+ * this program prints "programming is like a building puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art, \n");
+	return (0);
+}
