761035b00c9b68c8d54d2f07b51094b60cea5883
Felix <felisparkz@gmail.com>
2022-09-08 23:20:49 -0700

    assembled

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,37 @@
+#include <stdio.h>                                                                                                                                              
+
+                                                                                                                                                                
+
+/**                                                                                                                                                             
+
+*main - print the string in the  function                                                                                                                       
+
+*                                                                                                                                                               
+
+*Description: using the main function                                                                                                                           
+
+*this program prints "programming is like building amultilingual puzzle                                                                                         
+
+*Return: 0                                                                                                                                                      
+
+*/                                                                                                                                                              
+
+int main(void)                                                                                                                                                  
+
+{                                                                                                                                                               
+
+        char c;                                                                                                                                                 
+
+        int i;                                                                                                                                                  
+
+        long li;                                                                                                                                                
+
+        long long lli;                                                                                                                                          
+
+        float f;                                                                                                                                                
+
+                                                                                                                                                                
+
+        printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	prinf("Size of a long: %ld byte(s)\n", sizeof(li));
