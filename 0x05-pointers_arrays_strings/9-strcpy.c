efb3b9ca85b185a3bfd273f269141bf8b3279beb
Felix <felisparkz@gmail.com>
2022-09-20 14:19:27 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string unto the first argument
+ * @dest: this is destiny
+ * @soc: this is the copia
+ *
+ * Return: this return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
