534607a8721d0cebf028d76efbc9cc129ee8e5fa
Felix <felisparkz@gmail.com>
2022-09-19 18:54:16 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * strlen - function that returns the length of a string
+ * @s: string to be checked
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+	return (index);
+}
+
