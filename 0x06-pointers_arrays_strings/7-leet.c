26b6e545f3575a4d26e24312ca3439b4b3cf1ec8
Felix <felisparkz@gmail.com>
2022-09-23 21:42:28 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,10 +16,10 @@ char *leet(char *n)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == A1[i])
-			{
-				n[i] = A2[j];
-			}
+		if (n[i] == A1[i])
+		{
+			n[i] = A2[j];
+		}
 		}
 	}
 	return (n);
