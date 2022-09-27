8b7d08c62e2c9b0a775a8d16ef846a8331d9548f
Felix <felisparkz@gmail.com>
2022-09-26 20:26:08 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: source string
  * @b: the contant byte for filling
  * @n: length of buffer
- * Return: new string
+ * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
