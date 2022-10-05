b5fdf06835540e3eb86c162000a1cf4da9d46e83
Felix <felisparkz@gmail.com>
2022-10-05 08:27:12 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -9,33 +10,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i, ci;
+	int i_s1, i_s2, len1, len2;
+	char *a;
 
-	if (s1 == NULL)
+	if (s1 == 0)
+	{
 		s1 = "";
-	if (s2 == NULL)
+	}
+	for (len1 = 0; s1[len1] != 0; len1++)
+	{
+	}
+	if (s2 == 0)
+	{
 		s2 = "";
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	concat = malloc(sizeof(char) * (i + ci + 1));
-	if (concat == NULL)
-		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		concat[i] = s1[i];
-		i++;
 	}
-	while (s2[ci] != '\0')
+	for (len2 = 0; s2[len2] != 0; len2++)
 	{
-		concat[i] = s2[i];
-		i++, ci++;
 	}
-	concat[i] = '\0';
-	return (concat);
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (a == 0)
+	{
+		return (0);
+	}
+	for (i_s1 = 0; i_s1 < len1; i_s1++)
+	{
+		a[i_s1] = s1[i_s1];
+	}
+	for (i_s2 = 0; i_s2 < len2; i_s1++, i_s2++)
+	{
+		a[i_s1] = s2[i_s2];
+	}
+	a[i_s1] = 0;
+	return (a);
 }
-
