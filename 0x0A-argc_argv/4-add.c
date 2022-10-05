176f8b7b4d78d6deb6fed6de6d653a0e584e5835
Felix <felisparkz@gmail.com>
2022-10-05 08:52:58 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,8 +5,7 @@
  * main - entry point
  * Description: Adds positive numbers
  * @argc: amount of arguments passed through the program
- * @argv: pointer that contains the arrays of the arguments
- * 
+ * @argv: pointer that contains the arrays of the arguments 
  * Return: the sum of the numbers, if no number is passed then print 0, if a
  * number has symbols that are not digit then print Error and return 1
  */
