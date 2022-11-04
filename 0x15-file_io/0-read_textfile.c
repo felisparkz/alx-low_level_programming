5f6bb174579eddb3c166d19f761e2528c9cd4f9f
felisparkz <felisparkz@gmail.com>
2022-11-04 00:04:48 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,36 +4,41 @@
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of file to be read
  * @letters: number of letters to print
- * Return: the actual number of letters it could read and print
+ * Return: 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t print = 0, rd = 0;
-	int fd = 0;
-	char *buf = malloc(letters);
+	int fd, rd, wr;
+	int *buf;
 
-	if (!buf)
+	if (filename == NULL)
+	{
 		return (0);
-	if (!filename)
+	}
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
 		return (0);
-	fd = open(filename, O_RDONLY, 0600);
+	}
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	rd = read(fd, buf, letters);
 	if (rd == -1)
 	{
-		free(buf);
 		close(fd);
+		free(buf);
 		return (0);
 	}
-	print = write(STDOUT_FILENO, buf, rd);
-	if (print == -1 || print != rd)
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
 	{
-		free(buf);
-		close(fd);
 		return (0);
 	}
-	free(buf);
 	close(fd);
-	return (print);
-}
+	free(buf);
+	return (wr);
+}	
